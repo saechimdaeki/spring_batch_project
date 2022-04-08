059a40eb3f70aefdfcbc9fb08481f59f2805225e
김준성 <anima94@naver.com>
2022-04-08 20:00:54 +0900

    project setting

---
+++ src/test/java/me/saechimdaeki/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package me.saechimdaeki.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
